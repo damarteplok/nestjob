67be8df28b822e6fa13ea06cb0f28cd80edcfcc2
damarteplok <damarhuda@live.com>
2025-03-01 20:48:29 +0700

    test

--- apps/jobber-auth/src/app/app.module.ts
+++ apps/jobber-auth/src/app/app.module.ts
@@ -3,15 +3,20 @@ import { PrismaModule } from './prisma/prisma.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { UserModule } from './users/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
+    ConfigModule,
     PrismaModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
+      context: ({ req, res }) => ({ req, res }),
       autoSchemaFile: true,
     }),
     UserModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
---
+++ apps/jobber-auth/src/app/auth/auth.service.ts
@@ -0,0 +1,49 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { LoginInput } from './dto/login.input';
+import { Response } from 'express';
+import { UserService } from '../users/user.service';
+import { compare } from 'bcryptjs';
+import { ConfigService } from '@nestjs/config';
+import ms from 'ms';
+import { TokenPayload } from './toke-payload.interface';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly userService: UserService,
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService
+  ) {}
+
+  async login({ email, password }: LoginInput, response: Response) {
+    const user = await this.verifyUser(email, password);
+    const expires = new Date();
+    expires.setMilliseconds(
+      expires.getTime() + ms(this.configService.getOrThrow('JWT_EXPIRES_IN'))
+    );
+    const tokenPayload: TokenPayload = {
+      userId: user.id,
+    };
+    const accessToken = this.jwtService.sign(tokenPayload);
+    response.cookie('Authentication', accessToken, {
+      expires,
+      httpOnly: true,
+      secure: this.configService.get('NODE_ENV') === 'production',
+    });
+    return user;
+  }
+
+  private async verifyUser(email: string, password: string) {
+    try {
+      const user = await this.userService.getUser({ email });
+      const authenticated = await compare(password, user.password);
+      if (!authenticated) {
+        throw new UnauthorizedException();
+      }
+      return user;
+    } catch (err) {
+      throw new UnauthorizedException('Credentials are not valid');
+    }
+  }
+}
